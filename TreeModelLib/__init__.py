10dd73560aa581f915bb6fdadd4905bb8c1fcdf9
bathmann <jasper.bathmann@ufz.de>
2019-11-03 18:29:05 +0100

    [TML] init updated

---
+++ TreeModelLib/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  8 15:25:03 2018
+
+@author: bathmann
+"""
+
+if __name__ == '__main__' :
+    from AbovegroundCompetition import AbovegroundCompetition
+    from BelowgroundCompetition import BelowgroundCompetition
+    from GrowthAndDeathDynamics import GrowthAndDeathDynamics
+else:
+    from .AbovegroundCompetition import AbovegroundCompetition
+    from .BelowgroundCompetition import BelowgroundCompetition
+    from .GrowthAndDeathDynamics import GrowthAndDeathDynamics

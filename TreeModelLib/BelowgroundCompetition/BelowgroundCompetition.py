e85968fdbc1e970c13c64f313cdc7b4841ba3228
bathmann <jasper.bathmann@ufz.de>
2019-11-03 18:33:33 +0100

    [PL/BC] Simple test call corrected

--- TreeModelLib/BelowgroundCompetition/BelowgroundCompetition.py
+++ TreeModelLib/BelowgroundCompetition/BelowgroundCompetition.py
@@ -8,8 +8,14 @@ class BelowgroundCompetition:
     #  @date: 2019 - Today
     #  @author: jasper.bathmann@ufz.de
 
-    def __init__(self, case):
+    def __init__(self, args):
+        case = args.find("type").text
         if case == "SimpleTest":
-            return 0
+            self.iniSimpleTest(args)
         else:
-            raise KeyError("Required competition not implemented.")
+            raise KeyError("Required belowground competition not implemented.")
+        print(case + " belowground competition concept initiated.")
+
+    def iniSimpleTest(self, args):
+        from .SimpleTest import SimpleTest
+        self.case = args.find("type").text

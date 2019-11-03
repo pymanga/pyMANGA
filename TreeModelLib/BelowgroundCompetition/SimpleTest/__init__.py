8ae987b9869a20f415306fbdc6ea83770d486608
bathmann <jasper.bathmann@ufz.de>
2019-11-03 18:27:42 +0100

    [TML/BC] init updated

---
+++ TreeModelLib/BelowgroundCompetition/SimpleTest/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  8 15:25:03 2018
+
+@author: bathmann
+"""
+
+if __name__ == '__main__' :
+    import SimpleTest
+else:
+    from . import SimpleTest

addb6ecc7f0d80326b29115fd108c9d831ad4d95
bathmann <jasper.bathmann@ufz.de>
2019-11-03 18:26:45 +0100

    [TML] Simple Test for abovegroundcompetition implemented

--- TreeModelLib/AbovegroundCompetition/AbovegroundCompetition.py
+++ TreeModelLib/AbovegroundCompetition/AbovegroundCompetition.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+from lxml import etree
 
 class AbovegroundCompetition:
     ## Concept for aboveground competition
@@ -8,8 +8,14 @@ class AbovegroundCompetition:
     #  @date: 2019 - Today
     #  @author: jasper.bathmann@ufz.de
 
-    def __init__(self, case):
+    def __init__(self, args):
+        case = args.find("type").text
         if case == "SimpleTest":
-            return 0
+            self.iniSimpleTest(args)
         else:
-            raise KeyError("Required competition not implemented.")
+            raise KeyError("Required aboveground competition not implemented.")
+        print(case + " aboveground competition concept initiated.")
+
+    def iniSimpleTest(self, args):
+        from .SimpleTest import SimpleTest
+        self.case = args.find("type").text
---
+++ TreeModelLib/AbovegroundCompetition/SimpleTest/SimpleTest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@date: 2018-Today
+@author: jasper.bathmann@ufz.de
+"""
+
+class SimpleTest(object):
+    def __init__(self):
+        print("")
+
+    def calculateAbovegroundResources(self):
+        return 1
+
+    def updateAbovegoundCompetition(self, positions, geometries, parameters):
+        return 1

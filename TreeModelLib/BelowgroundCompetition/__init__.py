069cea85843883ab4aaec536e895857106612609
bathmann <jasper.bathmann@ufz.de>
2019-11-03 18:33:33 +0100

    [PL/BC] Simple test call corrected

--- TreeModelLib/BelowgroundCompetition/__init__.py
+++ TreeModelLib/BelowgroundCompetition/__init__.py
@@ -5,4 +5,9 @@ Created on Thu Nov  8 15:25:03 2018
 
 @author: bathmann
 """
-from . import *
+if __name__ == '__main__' :
+    from SimpleTest import SimpleTest
+    import BelowgroundCompetition
+else:
+    from .SimpleTest import SimpleTest
+    from . import BelowgroundCompetition

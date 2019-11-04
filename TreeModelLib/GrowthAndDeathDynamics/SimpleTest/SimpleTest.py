31172369182749250510fcc11334e008163debd0
bathmann <jasper.bathmann@ufz.de>
2019-11-04 17:20:02 +0100

    [TML/GAD] Simple test for GAD introduced

--- TreeModelLib/GrowthAndDeathDynamics/SimpleTest/SimpleTest.py
+++ TreeModelLib/GrowthAndDeathDynamics/SimpleTest/SimpleTest.py
@@ -7,11 +7,27 @@
 
 
 class SimpleTest(object):
-    def __init__(self):
-        print("")
+    def __init__(self, args):
+        ## SimpleTest case for death and growth dynamics. This case is
+        #  defined to test the passing of information between the instances.
+        #  @VAR: Tags to define SimpleTest: type
+        #  @date: 2019 - Today
+        case = args.find("type").text
+        print("Initiate belowground competition of type " + case + ".")
 
-    def calculateAbovegroundResources(self):
-        return 1
+    def prepareNextTimeStep(self, t_ini, t_end):
+        ## This functions prepares the cgrowth and death concept.
+        #  In the SimpleTest concept, trees are saved in a simple list
+        #  and the timestepping is updated. In preparation for the next time-
+        #  step, the list is simply resetted.
+        #  @VAR: t_ini - initial time for next timestep \n
+        #  t_end - end time for next timestep
+        self.trees = []
+        self.t_ini = t_ini
+        self.t_end = t_end
 
-    def updateAbovegoundCompetition(self, positions, geometries, parameters):
-        return 1
+    def progressTree(self, tree, aboveground_resources, belowground_resources):
+        geometry = tree.getGeometry()
+        parameter = tree.getParameter()
+        tree.setGeometry(geometry)
+        tree.setSurvival(1)

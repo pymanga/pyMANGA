4b543502c6246f3c3842f749ff3b3bbba3ebc1e4
bathmann <jasper.bathmann@ufz.de>
2019-11-04 11:17:55 +0100

    [TML/AC] Simple Test prepared for all functionalities

--- TreeModelLib/AbovegroundCompetition/AbovegroundCompetition.py
+++ TreeModelLib/AbovegroundCompetition/AbovegroundCompetition.py
@@ -4,7 +4,7 @@ from lxml import etree
 
 class AbovegroundCompetition:
     ## Concept for aboveground competition
-    #  @VAR case: aboveground competition concept to be used for the model
+    #  @VAR case: aboveground competition concept to be used for the model.
     #  @date: 2019 - Today
     #  @author: jasper.bathmann@ufz.de
 
@@ -14,8 +14,23 @@ class AbovegroundCompetition:
             self.iniSimpleTest(args)
         else:
             raise KeyError("Required aboveground competition not implemented.")
-        print(case + " aboveground competition concept initiated.")
+        print(case +
+              " aboveground competition successfully initiated.")
 
     def iniSimpleTest(self, args):
         from .SimpleTest import SimpleTest
-        self.case = args.find("type").text
+        self.concept = SimpleTest.SimpleTest(args)
+
+    def addTree(self, x, y, r_root, h_root, r_stem, h_stem, r_crown, kwargs):
+        self.concept.addTree(x, y, r_root, h_root, r_stem, h_stem, r_crown,
+                             kwargs)
+
+    def getAbovegroundResources(self):
+        return self.aboveground_resources
+
+    def calculateAbovegroundResources(self):
+        self.aboveground_resources = (
+                self.concept.calculateAbovegroundResources())
+
+    def prepareNextTimeStep(self, t_ini, t_end):
+        self.concept.prepareNextTimeStep(t_ini, t_end)

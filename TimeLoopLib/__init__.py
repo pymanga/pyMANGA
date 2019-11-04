0e380d03211b71a8db994263c5c8f53d7b3947aa
bathmann <jasper.bathmann@ufz.de>
2019-11-04 18:41:31 +0100

    [TLL] Timelooplib added

---
+++ TimeLoopLib/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  8 15:25:03 2018
+
+@author: bathmann
+"""
+if __name__ == '__main__':
+    import TreeDynamicTimeStepping
+    import TreeDynamicTimeLoop
+else:
+    from . import TreeDynamicTimeStepping
+    from . import TreeDynamicTimeLoop

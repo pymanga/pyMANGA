d04f8ee75618e34ce4a9be295478f74f00f92032
bathmann <jasper.bathmann@ufz.de>
2019-11-08 17:36:12 +0100

    [PopLib] Population as a child of treegroup

--- PopulationLib/__init__.py
+++ PopulationLib/__init__.py
@@ -6,8 +6,8 @@ Created on Thu Nov  8 15:25:03 2018
 @author: bathmann
 """
 
-from .Population import Population
 from .GroupPlanting import GroupPlanting
 from .TreeGroup import TreeGroup
+from .Population import Population
 from .Tree import Tree
 from .Species import Avicennia

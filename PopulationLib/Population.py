b27154bba10a3d7853e7f6f8395df5cac4917d0f
bathmann <jasper.bathmann@ufz.de>
2019-11-08 17:36:12 +0100

    [PopLib] Population as a child of treegroup

--- PopulationLib/Population.py
+++ PopulationLib/Population.py
@@ -5,20 +5,23 @@
 @author: jasper.bathmann@ufz.de
 """
 
-import PopulationLib
+import PopulationLib as PLib
+from PopulationLib import TreeGroup
 
 
-class Population:
+class Population(TreeGroup):
     def __init__(self, args):
         self.tree_groups = {}
-
+        self.trees = []
+        self.max_id = 0
         for arg in args.iter("group"):
             self.addTreeGroup(arg)
 
     def addTreeGroup(self, args):
-        tree_group_creator = PopulationLib.GroupPlanting(args)
+        tree_group_creator = PLib.GroupPlanting(args)
         tree_group = tree_group_creator.getGroup()
         self.tree_groups[tree_group.name] = tree_group
+        self.max_id += tree_group.getNumberOfTrees()
 
     def getTreeGroups(self):
         return self.tree_groups
@@ -26,9 +29,17 @@ class Population:
     def getTreeGroup(self, name):
         return self.tree_groups[name]
 
-    def getAllTrees(self):
+    def getTrees(self):
         all_trees = []
         for name, group in self.tree_groups.items():
             for tree in group.getTrees():
                 all_trees.append(tree)
+        all_trees.append(self.trees)
         return all_trees
+
+    def getNumberOfTrees(self):
+        n_trees = 0
+        for name, group in self.tree_groups.items():
+            n_trees += group.getNumberOfTrees()
+        n_trees += len(self.trees)
+        return n_trees

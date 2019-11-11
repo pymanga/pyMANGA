57862ab073c2cb48a01c71d20be59b6e3c9833d0
bathmann <jasper.bathmann@ufz.de>
2019-11-11 17:48:26 +0100

    [VisLib] Option implemented to choose no visualization

---
+++ VisualizationLib/NoVis/NoVis.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@date: 2018-Today
+@author: jasper.bathmann@ufz.de
+"""
+from VisualizationLib.Visualization import Visualization
+
+
+class NoVis(Visualization):
+    def __init__(self, args):
+        self.case = args.find("type").text
+        print("Running without visualization.")
+
+    def update(self, tree_groups, time):
+        pass
+
+    def show(self, time):
+        pass

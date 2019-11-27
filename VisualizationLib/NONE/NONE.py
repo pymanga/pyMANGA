0095d8e1660d0e9c1b0c1b19f4318a800c2d14ff
bathmann <jasper.bathmann@ufz.de>
2019-11-27 17:15:50 +0100

    [DOCU] Documentation for NONE case added

--- VisualizationLib/NONE/NONE.py
+++ VisualizationLib/NONE/NONE.py
@@ -8,12 +8,15 @@ from VisualizationLib.Visualization import Visualization
 
 
 class NONE(Visualization):
+    ## Constructor of dummy objects in order to drop visualization
     def __init__(self, args):
         self.case = args.find("type").text
         print("Running without visualization.")
 
+    ## Dummy update function
     def update(self, tree_groups, time):
         pass
 
+    ## Dummy show function
     def show(self, time):
         pass

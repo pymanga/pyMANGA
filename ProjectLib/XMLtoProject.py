3a2ef960108d65275cf8789e222808a0883ab0fb
marie <mcwimm@bitbucket.org>
2019-11-29 08:49:32 +0100

    [ProjectLib] added method to check if all required elements are included in the prj-file

--- ProjectLib/XMLtoProject.py
+++ ProjectLib/XMLtoProject.py
@@ -35,6 +35,41 @@ class XMLtoProject(Project.MangaProject):
 
         self.argsToProject()
 
+def checkRequiredElements(self):
+        # required elements on level1, ....
+        level1 = ["tree_dynamics", "initial_population",
+                  "tree_time_loop"]  # , "visualization"]
+
+        level2 = {"tree_dynamics": ["aboveground_competition",
+                                    "belowground_competition",
+                                    "tree_growth_and_death"],
+                  "initial_population": ["group"],
+                  "tree_time_loop": ["type",
+                                     "t_start",
+                                     "t_end"]}
+
+        level3 = {"tree_dynamics": ["type"],
+                  "initial_population": ["name",
+                                         "species",
+                                         "distribution"]}
+        for l1 in level1:
+            L1 = self.root.find(l1)
+            if L1 is None:
+                raise KeyError("Element '" + l1 + "' is missing! ")
+
+            for l2 in level2[l1]:
+                L2 = L1.find(l2)
+                if L2 is None:
+                    raise KeyError("Element '" + l2 + "' in '" +
+                                   l1 + "' is missing!")
+
+                if l1 in level3:
+                    for l3 in level3[l1]:
+                        L3 = L2.find(l3)
+                        if L3 is None:
+                            raise KeyError("Element '" + l3 + "' in '" +
+                                   l1 + "/" + l2 + "' is missing!")
+
     def readProjectFile(self):
         tree = etree.parse(self.prjfile)
         self.xmlTextStrip(tree)

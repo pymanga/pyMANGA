49991bc304746836ddcb12f0521ea9123b0e558b
bathmann <jasper.bathmann@ufz.de>
2019-12-02 11:55:35 +0100

    [TML/BC] Cell finder works now with a hack

--- TreeModelLib/BelowgroundCompetition/OGSLargeScale3D/OGSLargeScale3D.py
+++ TreeModelLib/BelowgroundCompetition/OGSLargeScale3D/OGSLargeScale3D.py
@@ -9,6 +9,7 @@ from TreeModelLib.BelowgroundCompetition import BelowgroundCompetition
 import vtk as vtk
 from lxml import etree
 from os import path
+import time
 
 
 class OGSLargeScale3D(BelowgroundCompetition):
@@ -21,26 +22,30 @@ class OGSLargeScale3D(BelowgroundCompetition):
         self.ogs_project_folder = args.find("ogs_project_folder").text.strip()
         self.ogs_project_file = args.find("ogs_project_file").text.strip()
         self.ogs_source_mesh = args.find("source_mesh").text.strip()
-        self.tree = etree.parse(path.join(self.ogs_project_folder,self.ogs_project_file))
+        self.tree = etree.parse(
+            path.join(self.ogs_project_folder, self.ogs_project_file))
         time_loop = self.tree.find("time_loop")
         time_loop__processes = time_loop.find("processes")
         time_loop__processes__process = time_loop__processes.find("process")
         time_stepping = time_loop__processes__process.find("time_stepping")
         self.xml_t_initial = time_stepping.find("t_initial")
         self.xml_t_end = time_stepping.find("t_end")
-        self.cell_information = CellInformation(path.join(self.ogs_project_folder,self.ogs_source_mesh))
+        self.cell_information = CellInformation(
+            path.join(self.ogs_project_folder, self.ogs_source_mesh))
         self.volumes = self.cell_information.getCellVolumes()
         self.tree.write("test.xml")
         self.source_mesh_name = args.find("source_mesh").text
         print("Initiate belowground competition of type " + case + ".")
+        self.start = time.time()
 
     def calculateBelowgroundResources(self):
         ## This function returns the BelowgroundResources calculated in the
         #  subsequent timestep. In the SimpleTest concept, for each tree a one
         #  is returned
         #  @return: np.array with $N_tree$ scalars
+        self.end = time.time()
+        print("time", self.end - self.start)
         exit()
-
 
     def prepareNextTimeStep(self, t_ini, t_end):
         ## This functions prepares the competition concept for the competition
@@ -67,21 +72,20 @@ class OGSLargeScale3D(BelowgroundCompetition):
         #  Here, in the SimpleTest case, each tree is represented by a one. In
         #  general, an object containing all necessary information should be
         #  stored for each tree
-        #  @VAR: position, geometry, parameter
-        self.i +=1
+        #  @para: position, geometry, parameter
+        self.i += 1
         self.trees_to_mesh_cell_id_map.append(1)
-        affected_cells = self.cell_information.getCellIDsAtXY(x,y)
+        affected_cells = self.cell_information.getCellIDsAtXY(x, y)
         v = 0
-
+        self.tree_cell_ids[affected_cells]
         for cell_id in affected_cells:
             v_i = self.volumes.GetTuple(cell_id)[0]
 
-            v+=v_i
+            v += v_i
 
         for cell_id in affected_cells:
             self.constant_contributions[cell_id] += .1
             self.salinity_prefactors[cell_id] += .1
-
 
 
 class CellInformation:
@@ -90,6 +94,7 @@ class CellInformation:
         meshReader.SetFileName(source_mesh)
         meshReader.Update()
         self.grid = meshReader.GetOutput()
+        self.cells = self.grid.GetCells()
         self.cell_finder = vtk.vtkCellLocator()
         self.cell_finder.SetDataSet(self.grid)
         self.cell_finder.LazyEvaluationOn()
@@ -100,30 +105,48 @@ class CellInformation:
         cell_id = self.cell_finder.FindCell([x, y, z])
         return cell_id
 
-## This function returns all the cell ids of the grid at a given x,y coordinate
-#  At the moment, it is only implemented as a "hackish" thing. Suggestions for
-#  improvements are most welcome.
+    ## This function returns all the cell ids of the grid at a given x,y-
+    #  coordinate. At the moment, the implementation is a bit weird.
+    #  Suggestions to improveme the implementation are most welcome.
     def getCellIDsAtXY(self, x, y):
-        p1 = [x, y, 22]
-        p2 = [x, y, self.grid.GetBounds()[-2]]
-        def linepoint(y):
-            return np.array(p1) + y * (np.array(p2) - np.array(p1))
+        bounds = self.grid.GetBounds()
+        p1 = [x, y, bounds[-1]]
+        p2 = [x, y, bounds[-2]]
+        cell_ids = vtk.vtkIdList()
+        self.cell_finder.FindCellsAlongLine(p1, p2, 1, cell_ids)
+
+        def linepoints(y):
+            return np.array(p1)[np.newaxis, :] + y[:, np.newaxis] * (
+                np.array(p2)[np.newaxis, :] - np.array(p1)[np.newaxis, :])
+
         search = 1
-        i = 0
+        yi = np.array([0, .5, 1])
+        points = (linepoints(yi))
         ids = []
-        while(search):
-            xi = np.random.uniform()
-            point = (linepoint(xi))
-            iD = (self.getCellId(point[0],point[1],point[2]))
+        i = 0
+        for point in points:
+            iD = (self.getCellId(point[0], point[1], point[2]))
             if (iD not in ids) and (iD != -1):
                 ids.append(iD)
-            i+=1
-            if len(ids)==3:
+        while (search):
+            new_yi = (yi[:-1] + yi[1:]) / 2.
+            points = linepoints(new_yi)
+            for point in points:
+                iD = (self.getCellId(point[0], point[1], point[2]))
+                if (iD not in ids) and (iD != -1):
+                    ids.append(iD)
+            i += 1
+            dist = np.abs((points[:-1] - points[1:]))[0, 2]
+            if len(ids) == 3:
                 search = 0
-            elif i == 1e9:
+            elif dist < 1e-4:
+                search = 0
+            elif i == 1e5:
                 search = 0
                 raise ValueError("Search algorithm failed! Please improve" +
                                  " algorithm!")
+            yi = np.concatenate((yi, new_yi))
+            yi.sort()
         return ids
 
     def getCellVolumeFromId(self, cell_id):
@@ -136,7 +159,3 @@ class CellInformation:
 
     def getCellVolumes(self):
         return self.volumes
-
-source = "/home/bathmann/Dokumente/UFZ/code/tmp/testproject/prj/source_domain.vtu"
-cell_finder = CellInformation(source)
-cell_finder.getCellIDsAtXY(55,50)

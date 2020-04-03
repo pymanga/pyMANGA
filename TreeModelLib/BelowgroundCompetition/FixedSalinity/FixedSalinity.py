4c5bb7bf673d96e199b32497adff16f552755f1a
Ronny <marzi_pan@bitbucket.org>
2020-04-03 16:32:40 +0200

    geyapft

--- TreeModelLib/BelowgroundCompetition/FixedSalinity/FixedSalinity.py
+++ TreeModelLib/BelowgroundCompetition/FixedSalinity/FixedSalinity.py
@@ -10,7 +10,7 @@ from TreeModelLib.BelowgroundCompetition import BelowgroundCompetition
 
 class FixedSalinity(BelowgroundCompetition):
     def __init__(self, args):
-        ## Fixed salinityin belowground competition concept. 
+        ## Fixed salinityin belowground competition concept.
         #  @param: Tags to define FixedSalinity: type, salinity
         #  @date: 2020 - Today
         case = args.find("type").text
@@ -18,15 +18,14 @@ class FixedSalinity(BelowgroundCompetition):
         self.GetSalinity(args)
 
     def calculateBelowgroundResources(self):
-        psi_zero = np.array(self._psi_leaf) + (2 * np.array(self._r_crown) + np.array(self._h_stem)) * 9810
-        psi_sali = np.array(psi_zero) + 85000000 * np.array(self._salinity_tree)
+        psi_zero = np.array(self._psi_leaf) + (2 * np.array(self._r_crown) +
+                                               np.array(self._h_stem)) * 9810
+        psi_sali = np.array(psi_zero) + 85000000 * np.array(
+            self._salinity_tree)
         self.belowground_resources = psi_sali / psi_zero
-  
 
     def GetSalinity(self, args):
-        missing_tags = [
-            "salinity", "type"
-        ]
+        missing_tags = ["salinity", "type"]
 
         for arg in args.iterdescendants():
             tag = arg.tag
@@ -57,5 +56,4 @@ class FixedSalinity(BelowgroundCompetition):
         self._h_stem = []
         self._r_crown = []
         self._psi_leaf = []
-        self._salinity_tree = [] 
-
+        self._salinity_tree = []

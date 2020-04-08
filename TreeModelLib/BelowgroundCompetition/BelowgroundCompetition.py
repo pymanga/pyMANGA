c33b5780fd66aa2d351a80cf88a00f99909c91ed
zaphod-bx <ronny.peters@tu-dresden.de>
2020-04-08 12:41:46 +0200

    Update BelowgroundCompetition.py

--- TreeModelLib/BelowgroundCompetition/BelowgroundCompetition.py
+++ TreeModelLib/BelowgroundCompetition/BelowgroundCompetition.py
@@ -21,8 +21,6 @@ class BelowgroundCompetition(TreeModel):
             self.iniFON(args)
         elif case == "FixedSalinity":
             self.iniFixedSalinity(args)
-        elif case == "SimpleHydro":
-            self.iniSimpleHydro(args)
         else:
             raise KeyError("Required belowground competition case " + case +
                            " not implemented.")
@@ -47,10 +45,6 @@ class BelowgroundCompetition(TreeModel):
     def iniFixedSalinity(self, args):
         from .FixedSalinity import FixedSalinity
         self.concept = FixedSalinity(args)
-
-    def iniSimpleHydro(self, args):
-        from .SimpleHydro import SimpleHydro
-        self.concept = SimpleHydro(args)
 
     def getBelowgroundResources(self):
         return self.belowground_resources

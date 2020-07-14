52d03a83f44d6751ec8faa9e4bbf201dc0d1ef8f
bathmann <jasper.bathmann@ufz.de>
2020-07-14 22:52:27 +0200

    yapf / formar

--- jsonconfig/jsonoptions.py
+++ jsonconfig/jsonoptions.py
@@ -1,5 +1,6 @@
 ''' Helper script for the pyManga.json notation.
-    def parse_jsontemplate: returns a python dictionary with the resolved dependencies.
+    def parse_jsontemplate: returns a python dictionary with the resolved
+    dependencies.
     def buildXML(option): returns xml string for a option object.
 '''
 import json
@@ -7,20 +8,21 @@ from xml.etree import ElementTree as ET
 from xml.dom import minidom
 import copy
 
-def dict_updateNew(data,add):
-    ''' Goes recursively throug dict "add" and adds 
+
+def dict_updateNew(data, add):
+    ''' Goes recursively throug dict "add" and adds
         all entries that are missing in dict "data".'''
-    for k,v in add.items():
+    for k, v in add.items():
         if k not in data:
             data[k] = add[k]
-        if isinstance(v,dict):
+        if isinstance(v, dict):
             subdict = data[k]
-            data[k] = dict_updateNew(subdict,v)
+            data[k] = dict_updateNew(subdict, v)
 
     return data
 
 
-def option_resolveReferences(option,optionMap,baseOptions):
+def option_resolveReferences(option, optionMap, baseOptions):
     # Replace ref with option if neccessary
     if "$ref" in option:
         ref = option["$ref"]
@@ -30,28 +32,29 @@ def option_resolveReferences(option,optionMap,baseOptions):
     # Insert missing parameters from base option if neccesary
     if "$base" in option:
         basetype = baseOptions[option["$base"]]
-        option = dict_updateNew(option,basetype)
+        option = dict_updateNew(option, basetype)
         del option["$base"]
 
     # Recursively go through suboptions in case of complex type
     if option["typedict"]["type"] == "complex":
         for subOption in option["value"]:
-            subOption = option_resolveReferences(subOption,optionMap,baseOptions)
-    
+            subOption = option_resolveReferences(subOption, optionMap,
+                                                 baseOptions)
+
     if option["typedict"]["type"] == "alternative":
         for subOption in option["typedict"]["alternatives"]:
-            subOption = option_resolveReferences(subOption,optionMap,baseOptions)
+            subOption = option_resolveReferences(subOption, optionMap,
+                                                 baseOptions)
 
     if "$namespace" in option:
         del option["$namespace"]
     return option
-    
 
 
 def fqn(option):
-    namespace = "{}:".format(option["$namespace"]) if "$namespace" in option else ""
-    return "{}{}".format(namespace,option["name"])
-
+    namespace = "{}:".format(
+        option["$namespace"]) if "$namespace" in option else ""
+    return "{}{}".format(namespace, option["name"])
 
 
 def parse_jsontemplate(path):
@@ -59,12 +62,11 @@ def parse_jsontemplate(path):
     with open(path) as jsonFile:
         d = dict(json.load(jsonFile))
         # Transform array to dict to make basetypes accesible by name
-        baseOptions = { data["name"]:data for data in d["base"] }
-        optionMap = { fqn(data):data for data in d["options"]}
+        baseOptions = {data["name"]: data for data in d["base"]}
+        optionMap = {fqn(data): data for data in d["options"]}
 
-        for name,option in optionMap.items():
-            option = option_resolveReferences(option,optionMap,baseOptions)
-
+        for name, option in optionMap.items():
+            option = option_resolveReferences(option, optionMap, baseOptions)
 
     return optionMap
 
@@ -72,11 +74,13 @@ def parse_jsontemplate(path):
 def buildNode(option):
     ''' Transform option in xml tree.
         option.name becomes a xml element tag.
-        option.value becomes element entry if the option is of type "integer", "float" or "string".
+        option.value becomes element entry if the option is of type "integer",
+        "float" or "string".
         option.value holds suboptions if option is of type "complex"
-        if option is of type "alternative", option.value is the index of the chosen alternative.
+        if option is of type "alternative", option.value is the index of the
+        chosen alternative.
     '''
-    name = option["name"]    
+    name = option["name"]
     if option["typedict"]["type"] == "complex":
         node = ET.Element(name)
         subnodes = list()
@@ -94,7 +98,7 @@ def buildNode(option):
     else:
         node = ET.Element(name)
         node.text = str(option["value"])
-    
+
     return node
 
 

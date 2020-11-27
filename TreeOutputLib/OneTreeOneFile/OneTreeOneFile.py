b97bcfb204cfa278cedf70ded714e0de0f5a494c
bathmann <jasper.bathmann@ufz.de>
2020-11-27 13:19:51 +0100

    [TOL] Changing heritage structure in order to safe lines of code

--- TreeOutputLib/OneTreeOneFile/OneTreeOneFile.py
+++ TreeOutputLib/OneTreeOneFile/OneTreeOneFile.py
@@ -4,59 +4,14 @@
 @date: 2018-Today
 @author: jasper.bathmann@ufz.de
 """
-from TreeOutputLib.TreeOutput import TreeOutput
+from TreeOutputLib.OneTimestepOneFile.OneTimestepOneFile import OneTimestepOneFile
 import os
 
 
 ## Output class. This class creates one file per tree at a defined location.
 #  A line containing time, position, desired geometric measures and desired
 #  parameters is written at every nth timestep.
-class OneTreeOneFile(TreeOutput):
-    ## Constructor of dummy objects in order to drop output
-    #  @param args xml element parsed from project to this constructor.
-    def __init__(self, args):
-        ## Directory, where output is saved. Please make sure it exists and is
-        #  empty.
-        self.output_dir = self.checkRequiredKey("output_dir", args)
-        ## N-timesteps between two outputs
-        self.output_each_nth_timestep = int(
-            self.checkRequiredKey("output_each_nth_timestep", args))
-        ## Check if overwrite of previous output is allowed
-        allow_previous_output = args.find("allow_previous_output")
-        if allow_previous_output is not None:
-            allow_previous_output = bool(allow_previous_output.text)
-        else: allow_previous_output = False
-        ## Geometric measures included in output
-        self.geometry_outputs = []
-        ## Parameters included in output
-        self.parameter_outputs = []
-        ## Parameters included in output
-        self.growth_outputs = []
-        ## Counter for output generation
-        self._output_counter = 0
-        for key in args.iterchildren("geometry_output"):
-            self.geometry_outputs.append(key.text.strip())
-        for key in args.iterchildren("parameter_output"):
-            self.parameter_outputs.append(key.text.strip())
-        for key in args.iterchildren("growth_output"):
-            self.growth_outputs.append(key.text.strip())
-        try:
-            dir_files = len(os.listdir(self.output_dir))
-        except FileNotFoundError:
-            raise FileNotFoundError(
-                "[Errno 2] No such directory: '" + self.output_dir +
-                "' as defined in the project file." +
-                " Please make sure your output directory exists!")
-        if (dir_files > 0 and allow_previous_output == False):
-            raise ValueError("Output directory '" + self.output_dir +
-                             "' is not empty.")
-        print(
-            "Output to '" + self.output_dir + "' of tree positions, the " +
-            "parameters ", self.parameter_outputs,
-            " and geometric" + " measures ", self.geometry_outputs,
-            " at every " + str(self.output_each_nth_timestep) +
-            " timesteps initialized.")
-
+class OneTreeOneFile(OneTimestepOneFile):
     ## Writes output to predefined folder
     #  For each tree a file is created and updated throughout the simulation.
     #  This function is only able to work, if the output directory exists and
@@ -109,21 +64,3 @@ class OneTreeOneFile(TreeOutput):
                     file.write(string)
                     file.close()
         self._output_counter += 1
-
-    ## This function checks if a key exists and if its text content is empty.
-    #  Raises key-errors, if the key is not properly defined.
-    #  @param key Name of the key to be checked
-    #  @param args args parsed from project. Xml-element
-    def checkRequiredKey(self, key, args):
-        tmp = args.find(key)
-        if tmp == None:
-            raise KeyError("Required key '" + key + "' in project file at " +
-                           "position MangaProject__tree_output is missing.")
-        elif tmp.text.strip() == "":
-            raise KeyError("Key '" + key + "' in project file at position " +
-                           "MangaProject__tree_output needs to be specified.")
-        return tmp.text
-
-    ## This function returns the output directory
-    def getOutputDir(self):
-        return self.output_dir

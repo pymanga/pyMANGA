e5377eefce3e64e750299c7eaa5d3ea69094ef9a
Ronny <marzi_pan@bitbucket.org>
2020-11-30 15:57:56 +0100

    TML/BC/SH geyapft

--- TreeModelLib/BelowgroundCompetition/SimpleHydro/SimpleHydro.py
+++ TreeModelLib/BelowgroundCompetition/SimpleHydro/SimpleHydro.py
@@ -25,81 +25,102 @@ class SimpleHydro(BelowgroundCompetition):
     def calculateBelowgroundResources(self):
         self.transpire()
         self.belowground_resources = ((np.array(self._potential_nosal) +
-                              np.array(self._salinity) * 85000)
-                             /np.array(self._potential_nosal))
+                                       np.array(self._salinity) * 85000) /
+                                      np.array(self._potential_nosal))
         print(self.belowground_resources)
-        
-
 
     def transpire(self):
-        distance = (np.array(self._r_root)[np.newaxis, np.newaxis, :] - 
-                      ((self._my_grid[0][:, :, np.newaxis] -
+        distance = (np.array(self._r_root)[np.newaxis, np.newaxis, :] -
+                    ((self._my_grid[0][:, :, np.newaxis] -
                       np.array(self._xe)[np.newaxis, np.newaxis, :])**2 +
-                      (self._my_grid[1][:, :, np.newaxis] -
+                     (self._my_grid[1][:, :, np.newaxis] -
                       np.array(self._ye)[np.newaxis, np.newaxis, :])**2)**0.5)
-        water_loss = (distance - distance)[:,:,0]
+        water_loss = (distance - distance)[:, :, 0]
         presence = distance > 0
-        maxe = np.amax(distance,axis=(0,1))
+        maxe = np.amax(distance, axis=(0, 1))
         self._salinity = []
         self.transpiration = []
         for ii in range(distance.shape[2]):
-           closest = distance[:,:,ii] == maxe[ii]
-           presence[:,:,ii] = np.fmax(closest, presence[:,:,ii])
-           self._salinity.append((np.sum( presence[:,:,ii] * self.salinity) /
-                       np.sum(presence[:,:,ii])))
-#           print(self._salinity)
-           self.transpiration.append((-self._potential_nosal[ii] - self._salinity[ii] 
-                       * 85000)/self._resistance[ii]/np.pi * (self._t_end - self._t_ini))
-           water_loss += (self.transpiration[ii] / np.sum(presence[:,:,ii])) * presence[:,:,ii]
+            closest = distance[:, :, ii] == maxe[ii]
+            presence[:, :, ii] = np.fmax(closest, presence[:, :, ii])
+            self._salinity.append((np.sum(presence[:, :, ii] * self.salinity) /
+                                   np.sum(presence[:, :, ii])))
+            self.transpiration.append(
+                (-self._potential_nosal[ii] - self._salinity[ii] * 85000) /
+                self._resistance[ii] / np.pi * (self._t_end - self._t_ini))
+            water_loss += (self.transpiration[ii] /
+                           np.sum(presence[:, :, ii])) * presence[:, :, ii]
         print(np.sum(self.transpiration))
-# dilution
-        self.salinity += self._sea_salinity*water_loss/self.volume
-        self.salinity += -self.salinity*self._dilution_frac_upper+self._sea_salinity*self._dilution_frac_upper
-         
-# diffusion
-        salinity_new = self.salinity * (1-self._diffusion_frac)
-   # x-dir
+        # dilution
+        self.salinity += self._sea_salinity * water_loss / self.volume
+        self.salinity += (-self.salinity * self._dilution_frac_upper +
+                          self._sea_salinity * self._dilution_frac_upper)
+
+        # diffusion
+        salinity_new = self.salinity * (1 - self._diffusion_frac)
+        # x-dir
         for ii in range(self.x_resolution):
-            if ii == self.x_resolution-1:
-                salinity_new[:,ii] += self.salinity[:,ii]*self._diffusion_frac/4
-                salinity_new[:,ii-1] += self.salinity[:,ii]*self._diffusion_frac/4
+            if ii == self.x_resolution - 1:
+                salinity_new[:,
+                             ii] += self.salinity[:,
+                                                  ii] * self._diffusion_frac / 4
+                salinity_new[:, ii -
+                             1] += self.salinity[:,
+                                                 ii] * self._diffusion_frac / 4
             elif ii == 0:
-                salinity_new[:,ii+1] += self.salinity[:,ii]*self._diffusion_frac/4
-                salinity_new[:,ii] += self.salinity[:,ii]*self._diffusion_frac/4
+                salinity_new[:, ii +
+                             1] += self.salinity[:,
+                                                 ii] * self._diffusion_frac / 4
+                salinity_new[:,
+                             ii] += self.salinity[:,
+                                                  ii] * self._diffusion_frac / 4
             else:
-                salinity_new[:,ii+1] += self.salinity[:,ii]*self._diffusion_frac/4
-                salinity_new[:,ii-1] += self.salinity[:,ii]*self._diffusion_frac/4
-   # y-dir
+                salinity_new[:, ii +
+                             1] += self.salinity[:,
+                                                 ii] * self._diffusion_frac / 4
+                salinity_new[:, ii -
+                             1] += self.salinity[:,
+                                                 ii] * self._diffusion_frac / 4
+
+        # y-dir
         for ii in range(self.y_resolution):
-            if ii == self.y_resolution-1:
-                salinity_new[ii,:] += self.salinity[ii,:]*self._diffusion_frac/4
-                salinity_new[ii-1,:] += self.salinity[ii,:]*self._diffusion_frac/4
+            if ii == self.y_resolution - 1:
+                salinity_new[
+                    ii, :] += self.salinity[ii, :] * self._diffusion_frac / 4
+                salinity_new[
+                    ii -
+                    1, :] += self.salinity[ii, :] * self._diffusion_frac / 4
             elif ii == 0:
-                salinity_new[ii+1,:] += self.salinity[ii,:]*self._diffusion_frac/4
-                salinity_new[ii,:] += self.salinity[ii,:]*self._diffusion_frac/4
+                salinity_new[
+                    ii +
+                    1, :] += self.salinity[ii, :] * self._diffusion_frac / 4
+                salinity_new[
+                    ii, :] += self.salinity[ii, :] * self._diffusion_frac / 4
             else:
-                salinity_new[ii+1,:] += self.salinity[ii,:]*self._diffusion_frac/4
-                salinity_new[ii-1,:] += self.salinity[ii,:]*self._diffusion_frac/4
+                salinity_new[
+                    ii +
+                    1, :] += self.salinity[ii, :] * self._diffusion_frac / 4
+                salinity_new[
+                    ii -
+                    1, :] += self.salinity[ii, :] * self._diffusion_frac / 4
         self.salinity = salinity_new
-# gradient-flow
-        multi_fac = self.Q_fac * (self._t_end - self._t_ini)       
-#        print(multi_fac) 
-        salinity_new[0,:] = self.salinity[0,:] * (1 - multi_fac)  + self._up_sal * multi_fac 
-        salinity_new[1:self.y_resolution,:] = (self.salinity[1:self.y_resolution,:]
-                              * (1 - multi_fac)  + self.salinity[0:(self.y_resolution-1),:] * multi_fac) 
-        self.salinity=salinity_new
+        # gradient-flow
+        multi_fac = self.Q_fac * (self._t_end - self._t_ini)
+        salinity_new[0, :] = self.salinity[0, :] * (
+            1 - multi_fac) + self._up_sal * multi_fac
+        salinity_new[1:self.y_resolution, :] = (
+            self.salinity[1:self.y_resolution, :] * (1 - multi_fac) +
+            self.salinity[0:(self.y_resolution - 1), :] * multi_fac)
+        self.salinity = salinity_new
         print(np.ndarray.round(self.salinity))
-
-
-
 
     ## This function initialises the mesh.\n
     def makeGrid(self, args):
         missing_tags = [
             "type", "domain", "x_1", "x_2", "y_1", "y_2", "x_resolution",
             "y_resolution", "depth", "porosity", "dilution_frac_upper",
-            "dilution_frac_lower", "diffusion_frac",
-            "sea_salinity", "ini_sal", "up_sal", "slope", "k_f"
+            "dilution_frac_lower", "diffusion_frac", "sea_salinity", "ini_sal",
+            "up_sal", "slope", "k_f"
         ]
         for arg in args.iterdescendants():
             tag = arg.tag
@@ -184,7 +205,7 @@ class SimpleHydro(BelowgroundCompetition):
         self._ye = []
         self._resistance = []
         self._potential_nosal = []
-        print(self._t_ini/3600/24/365)
+        print(self._t_ini / 3600 / 24 / 365)
 
     ## Before being able to calculate the resources, all tree entities need
     #  to be added with their relevant allometric measures for the next timestep.
@@ -205,8 +226,10 @@ class SimpleHydro(BelowgroundCompetition):
                                                 parameter["kf_sap"],
                                                 geometry["r_stem"])
         self._resistance.append(root_surface_resistance + xylem_resistance)
-        self._potential_nosal.append((parameter["leaf_water_potential"] 
-                               + (2 * geometry["r_crown"] + geometry["h_stem"]) * 9810))
+        self._potential_nosal.append(
+            (parameter["leaf_water_potential"] +
+             (2 * geometry["r_crown"] + geometry["h_stem"]) * 9810))
+
     ## This function calculates the root surface resistance.
     #  @param lp: lp value must exist in tree parameters
     #  @param k_geom: k_geom value must exist in tree parameters
@@ -227,4 +250,3 @@ class SimpleHydro(BelowgroundCompetition):
         flow_length = (2 * r_crown + h_stem + 0.5**0.5 * r_root)
         xylem_resistance = (flow_length / kf_sap / np.pi / r_stem**2)
         return xylem_resistance
-

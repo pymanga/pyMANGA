a88967706aff4c9e4bb3fc52bf619cc9395ceff7
marie <mcwimm@bitbucket.org>
2021-02-19 11:47:39 +0100

    [TML] call method progressTree from parent class

--- TreeModelLib/GrowthAndDeathDynamics/NetworkBettina/NetworkBettina.py
+++ TreeModelLib/GrowthAndDeathDynamics/NetworkBettina/NetworkBettina.py
@@ -18,55 +18,18 @@ class NetworkBettina(SimpleBettina):
         print("Growth and death dynamics of type " + case + ".")
 
     def progressTree(self, tree, aboveground_resources, belowground_resources):
-        # network
         network = tree.getNetwork()
         self.rgf = network['rgf']
         self.partner = network['partner']
         self.potential_partner = network['potential_partner']
         self.psi_osmo = network["psi_osmo"]
         self.name = str(tree.group_name) + str(tree.tree_id)
-        # network end
-        geometry = tree.getGeometry()
-        growth_concept_information = tree.getGrowthConceptInformation()
-        self.parameter = tree.getParameter()
-        self.r_crown = geometry["r_crown"]
-        self.h_crown = geometry["h_crown"]
-        self.r_root = geometry["r_root"]
-        self.h_root = geometry["h_root"]
-        self.r_stem = geometry["r_stem"]
-        self.h_stem = geometry["h_stem"]
-        self.survive = 1
-        self.flowLength()
-        self.treeVolume()
-        self.treeMaintenance()
-        self.bgResources(belowground_resources)
-        self.agResources(aboveground_resources)
-        self.growthResources()
-        self.treeGrowthWeights()
-        self.treeGrowth()
-        geometry["r_crown"] = self.r_crown
-        geometry["h_crown"] = self.h_crown
-        geometry["r_root"] = self.r_root
-        geometry["h_root"] = self.h_root
-        geometry["r_stem"] = self.r_stem
-        geometry["h_stem"] = self.h_stem
-        growth_concept_information["root_surface_resistance"] = self.root_surface_resistance
-        growth_concept_information["xylem_resistance"] = self.xylem_resistance
-        growth_concept_information["ag_resources"] = self.ag_resources
-        growth_concept_information["bg_resources"] = self.bg_resources
-        growth_concept_information["growth"] = self.grow
-        growth_concept_information["available_resources"] = self.available_resources
-        psi_zero = self.deltaPsi()
-        growth_concept_information["psi_zero"] = psi_zero
-        growth_concept_information["salinity"] = -self.psi_osmo / 85000000
-        tree.setGeometry(geometry)
-        tree.setGrowthConceptInformation(growth_concept_information)
-        # network
+        # Simple bettina tree progress
+        SimpleBettina.progressTree(self, tree, aboveground_resources, belowground_resources)
         network['rgf'] = self.rgf
         network['potential_partner'] = self.potential_partner
         network['partner'] = self.partner
         tree.setNetwork(network)
-        # network end
         if (self.survive == 1):
             tree.setSurvival(1)
         else:

c1b95fda3bc7a5f12a060c52fffb8d34764022ed
bathmann <jasper.bathmann@ufz.de>
2021-02-19 12:51:21 +0100

    [TOL] Delimiters changed, typo fixed

--- TreeOutputLib/OneTimestepOneFilePerGroup/OneTimestepOneFilePerGroup.py
+++ TreeOutputLib/OneTimestepOneFilePerGroup/OneTimestepOneFilePerGroup.py
@@ -14,7 +14,7 @@ class OneTimestepOneFilePerGroup(OneTimestepOneFile):
     ## Writes output to predefined folder
     #  For each timestep a file is created throughout the simulation.
     #  This function is only able to work, if the output directory exists and
-    #  is empty at the begin of the model run
+    #  is empty at the beginning of the model run
     def writeOutput(self, tree_groups, time):
         self._output_counter = (self._output_counter %
                                 self.output_each_nth_timestep)
@@ -23,33 +23,33 @@ class OneTimestepOneFilePerGroup(OneTimestepOneFile):
                 filename = (group_name + "_t_%012.1f" % (time) + ".csv")
                 file = open(self.output_dir + filename, "w")
                 string = ""
-                string += "tree,\t time,\t x,\t y"
+                string += "tree \t time \t x \t y"
                 for geometry_output in self.geometry_outputs:
-                    string += ",\t" + geometry_output
+                    string += "\t" + geometry_output
                 for parameter_output in self.parameter_outputs:
-                    string += ",\t" + parameter_output
+                    string += "\t" + parameter_output
                 for growth_output in self.growth_outputs:
-                    string += ",\t" + growth_output
+                    string += "\t" + growth_output
                 string += "\n"
                 file.write(string)
                 for tree in tree_group.getTrees():
                     growth_information = tree.getGrowthConceptInformation()
                     string = ""
                     string += (group_name + "_" + "%09.0d" % (tree.getId()) +
-                               ",\t" + str(time) + ",\t" + str(tree.x) +
-                               ",\t" + str(tree.y))
+                               "\t" + str(time) + "\t" + str(tree.x) +
+                               "\t" + str(tree.y))
                     if (len(self.geometry_outputs) > 0):
                         geometry = tree.getGeometry()
                         for geometry_output in self.geometry_outputs:
-                            string += ",\t" + str(geometry[geometry_output])
+                            string += "\t" + str(geometry[geometry_output])
                     if (len(self.parameter_outputs) > 0):
                         parameter = tree.getParameter()
                         for parameter_output in self.parameter_outputs:
-                            string += ",\t" + str(parameter[parameter_output])
+                            string += "\t" + str(parameter[parameter_output])
                     if (len(growth_information) > 0):
                         for growth_output_key in self.growth_outputs:
                             try:
-                                string += ",\t" + str(
+                                string += "\t" + str(
                                     growth_information[growth_output_key])
                             except KeyError:
                                 raise KeyError(
--- TreeOutputLib/OneTreeOneFile/OneTreeOneFile.py
+++ TreeOutputLib/OneTreeOneFile/OneTreeOneFile.py
@@ -29,30 +29,30 @@ class OneTreeOneFile(OneTimestepOneFile):
                     file = open(self.output_dir + filename, "a")
                     if filename not in files_in_folder:
                         string = ""
-                        string += "time,\t x ,\t y"
+                        string += "time \t x \t y"
                         for geometry_output in self.geometry_outputs:
-                            string += ",\t" + geometry_output
+                            string += "\t" + geometry_output
                         for parameter_output in self.parameter_outputs:
-                            string += ",\t" + parameter_output
+                            string += "\t" + parameter_output
                         for growth_output in self.growth_outputs:
-                            string += ",\t" + growth_output
+                            string += "\t" + growth_output
                         string += "\n"
                         file.write(string)
                     string = ""
-                    string += (str(time) + ",\t" + str(tree.x) + ",\t" +
+                    string += (str(time) + "\t" + str(tree.x) + "\t" +
                                str(tree.y))
                     if (len(self.geometry_outputs) > 0):
                         geometry = tree.getGeometry()
                         for geometry_output in self.geometry_outputs:
-                            string += ",\t" + str(geometry[geometry_output])
+                            string += "\t" + str(geometry[geometry_output])
                     if (len(self.parameter_outputs) > 0):
                         parameter = tree.getParameter()
                         for parameter_output in self.parameter_outputs:
-                            string += ",\t" + str(parameter[parameter_output])
+                            string += "\t" + str(parameter[parameter_output])
                     if (len(growth_information) > 0):
                         for growth_output_key in self.growth_outputs:
                             try:
-                                string += ",\t" + str(
+                                string += "\t" + str(
                                     growth_information[growth_output_key])
                             except KeyError:
                                 raise KeyError(

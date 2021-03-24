a98786d64a3bfd543a5a7ee74bcb797de0ecb32a
marie <mcwimm@bitbucket.org>
2021-03-24 17:57:04 +0100

    [PL] update network parameters

--- PopulationLib/Tree.py
+++ PopulationLib/Tree.py
@@ -88,16 +88,15 @@ class Tree:
         self.network['water_absorbed'] = []
         self.network['water_available'] = []
         self.network['water_exchanged'] = []
-        # List with minimum grafted root radius
-        self.network['r_gr_min'] = []
-        self.network['r_gr_rgf'] = []
-        self.network['r_gr'] = []
         ## List with lengths of grafted roots (proportional to r_root of
         # adjacent trees
-        self.network['l_gr_rgf'] = []
-        self.network['l_gr'] = []
         self.network['weight_gr'] = 0
         self.network['psi_osmo'] = []
+        # List with minimum grafted root radius, only for rgf variant V2
+        self.network['r_gr_min'] = []
+        self.network['r_gr_rgf'] = []
+        self.network['l_gr_rgf'] = []
+
 
     def getNetwork(self):
         return self.network

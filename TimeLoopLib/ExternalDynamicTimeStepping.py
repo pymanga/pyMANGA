3d56b30bd0fa71279ab1374b0b8209d0c45dee57
bathmann <jasper.bathmann@ufz.de>
2022-01-22 09:15:32 +0100

    [TLL] Update timestepping

--- TimeLoopLib/ExternalDynamicTimeStepping.py
+++ TimeLoopLib/ExternalDynamicTimeStepping.py
@@ -21,8 +21,20 @@ class ExternalDynamicTimeStepping(TreeDynamicTimeStepping):
                 """In order to use MANGA as OGS python boundary condition,
                     one has to use a corresponding belowground competition
                     concept. Please see documentation for further details!""")
+            
+    ## This functions sets the step size defined externally, as nth time
+    # step for each concept.
+    # If, for example, steps are defined as follows: n_step_ag = 2,
+    # n_step_bg = 1, n_step_gd = 1, then above-ground competition is updated
+    # every 2nd step while the other concepts are updated in each time step.
+    def setSteps(self, n_step_ag, n_step_bg, n_step_gd):
+        self.n_step_ag = n_step_ag
+        self.n_step_bg = n_step_bg
+        self.n_step_gd = n_step_gd
 
-    def step(self, t_end):
+    ## This progresses one time step, by updating tree population and above-
+    # and below-ground resources depending on the associated bools
+    def step(self, t_end, update_ag, update_bg, update_gd):
         t_start = self.t_step_begin
-        super().step(t_start, t_end)
+        super().step(t_start, t_end, update_ag, update_bg, update_gd)
         t_end = self.t_step_begin
--- TimeLoopLib/TreeDynamicTimeStepping.py
+++ TimeLoopLib/TreeDynamicTimeStepping.py
@@ -17,11 +17,15 @@ class TreeDynamicTimeStepping:
         ## Output configuration
         self.tree_output = project.getTreeOutput()
 
-    def step(self, t_start, t_end):
-        self.aboveground_competition.prepareNextTimeStep(t_start, t_end)
-        self.belowground_competition.prepareNextTimeStep(t_start, t_end)
-        self.death_and_growth_concept.prepareNextTimeStep(t_start, t_end)
+    def step(self, t_start, t_end, update_ag, update_bg, update_gd):
+        if update_ag:
+            self.aboveground_competition.prepareNextTimeStep(t_start, t_end)
+        if update_bg:
+            self.belowground_competition.prepareNextTimeStep(t_start, t_end)
+        if update_gd:
+            self.death_and_growth_concept.prepareNextTimeStep(t_start, t_end)
         tree_groups = self.population.getTreeGroups()
+        
         self.tree_output.writeOutput(tree_groups, t_start)
         for group_name, tree_group in tree_groups.items():
             for tree in tree_group.getTrees():
@@ -29,24 +33,37 @@ class TreeDynamicTimeStepping:
                 self.aboveground_competition.addTree(tree)
                 self.belowground_competition.addTree(tree)
 
-        self.aboveground_competition.calculateAbovegroundResources()
-        self.belowground_competition.calculateBelowgroundResources()
-        belowground_resources = (
-            self.belowground_competition.getBelowgroundResources())
-        aboveground_resources = (
-            self.aboveground_competition.getAbovegroundResources())
-        j = 0
-        for group_name, tree_group in tree_groups.items():
-            kill_indices = []
-            for tree, i in zip(tree_group.getTrees(),
-                               range(tree_group.getNumberOfTrees())):
-                self.death_and_growth_concept.progressTree(
-                    tree, aboveground_resources[j], belowground_resources[j])
-                if not tree.getSurvival():
-                    kill_indices.append(i)
-                j += 1
-            tree_group.removeTreesAtIndices(kill_indices)
-            tree_group.recruitTrees()
+        if update_ag:
+            self.aboveground_competition.calculateAbovegroundResources()
+            self.aboveground_resources = (
+                self.aboveground_competition.getAbovegroundResources())
+        if update_bg:
+            self.belowground_competition.calculateBelowgroundResources()
+            self.belowground_resources = (
+                self.belowground_competition.getBelowgroundResources())
+        if update_gd:
+            j = 0
+            for group_name, tree_group in tree_groups.items():
+                kill_indices = []
+                for tree, i in zip(tree_group.getTrees(),
+                                   range(tree_group.getNumberOfTrees())):
+                    # if a new tree is recruited in the current time step and
+                    # the respective resource was not updated, set survival of
+                    # the new tree to 1
+                    try:
+                        ag = self.aboveground_resources[j]
+                        bg = self.belowground_resources[j]
+                        self.death_and_growth_concept.progressTree(
+                            tree, ag, bg)
+                    except IndexError:
+                        tree.setSurvival(1)
+
+                    if not tree.getSurvival():
+                        kill_indices.append(i)
+
+                    j += 1
+                tree_group.removeTreesAtIndices(kill_indices)
+                tree_group.recruitTrees()
         self.visualization.update(tree_groups, t_end)
 
     ## Last action, when timeloop is done

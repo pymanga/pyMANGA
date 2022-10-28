68768c76754a7458902f22c536fc55a10981d960
jbathmann <jbathmann@posteo.net>
2022-10-28 10:13:03 +0200

    [Benchmarks] Review following suggestions by mcw

--- Benchmarks/ModuleBenchmarks/BelowgroundCompetition/OGSLargeScale3D/python_script.py
+++ Benchmarks/ModuleBenchmarks/BelowgroundCompetition/OGSLargeScale3D/python_script.py
@@ -1,8 +1,6 @@
 import OpenGeoSys
-import vtk as vtk
+# Numpy is required for pyMANGA
 import numpy as np
-from math import pi, sin
-import os
 
 def transectElevation(x, m=-0.1/22.):
     return float(m * x)
@@ -16,21 +14,29 @@ class BC_p_D(OpenGeoSys.BoundaryCondition):
     def getDirichletBCValue(self, t, coords, node_id, primary_vars):
         x, y, z = coords
         value = pressure_value(z, x)
+        # Returns two values. First is to indicate whether BC is 
+        # assembled. 2nd is providing the actual value
         return (True, value)
 
 ## Dirichlet BCs for concentration
 class BC_left_C_D(OpenGeoSys.BoundaryCondition):
 
     def getDirichletBCValue(self, t, coords, node_id, primary_vars):
-        x, y, z = coords
+        # Salinity is provided in the project file (abiotic_factors)
+        # Parsing is tested here
         value = left_salinity
+        # Returns two values. First is to indicate whether BC is 
+        # assembled. 2nd is providing the actual value
         return (True, value)
 
 class BC_right_C_D(OpenGeoSys.BoundaryCondition):
 
     def getDirichletBCValue(self, t, coords, node_id, primary_vars):
-        x, y, z = coords
+        # Salinity is provided in the project file (abiotic_factors)
+        # Parsing is tested here
         value = right_salinity
+        # Returns two values. First is to indicate whether BC is 
+        # assembled. 2nd is providing the actual value
         return (True, value)
 
 bc_left_p = BC_p_D()

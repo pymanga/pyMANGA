814d0be4f43ed3ab29a0c790fd588316c689cac1
marie <mc.wimmler@gmx.de>
2023-04-25 10:20:26 +0200

    [TOL] update output concepts to avoid empty files or files without header

--- TreeOutputLib/OneTimestepOneFilePerGroup/OneTimestepOneFilePerGroup.py
+++ TreeOutputLib/OneTimestepOneFilePerGroup/OneTimestepOneFilePerGroup.py
@@ -17,29 +17,30 @@ class OneTimestepOneFilePerGroup(OneTimestepOneFile):
     def outputContent(self, tree_groups, time, **kwargs):
         delimiter = "\t"
         for group_name, tree_group in tree_groups.items():
-            if not kwargs["group_died"]:
-                filename = (group_name + "_t_%012.1f" % (time) + ".csv")
-            else:
-                filename = (group_name + "_t_%012.1f_group_died" % (time) + ".csv")
+            if not tree_group.getNumberOfTrees() == 0:
+                if not kwargs["group_died"]:
+                    filename = (group_name + "_t_%012.1f" % (time) + ".csv")
+                else:
+                    filename = (group_name + "_t_%012.1f_group_died" % (time) + ".csv")
 
-            file = open(os.path.join(self.output_dir, filename), "w")
-            string = ""
-            string += 'tree' + delimiter + 'time' + delimiter + 'x' +  \
-                      delimiter + 'y'
-            string = OneTimestepOneFile.addSelectedHeadings(
-                self, string, delimiter)
-            string += "\n"
-            file.write(string)
-            for tree in tree_group.getTrees():
-                growth_information = tree.getGrowthConceptInformation()
+                file = open(os.path.join(self.output_dir, filename), "w")
                 string = ""
-                string += (group_name + "_" + "%09.0d" % (tree.getId()) +
-                           delimiter + str(time) + delimiter + str(tree.x) +
-                           delimiter + str(tree.y))
-                string = OneTimestepOneFile.addSelectedOutputs(
-                    self, tree, string, delimiter, growth_information)
+                string += 'tree' + delimiter + 'time' + delimiter + 'x' +  \
+                          delimiter + 'y'
+                string = OneTimestepOneFile.addSelectedHeadings(
+                    self, string, delimiter)
                 string += "\n"
                 file.write(string)
-                for growth_output in self.growth_outputs:
-                    del (growth_information[growth_output])
-            file.close()
+                for tree in tree_group.getTrees():
+                    growth_information = tree.getGrowthConceptInformation()
+                    string = ""
+                    string += (group_name + "_" + "%09.0d" % (tree.getId()) +
+                               delimiter + str(time) + delimiter + str(tree.x) +
+                               delimiter + str(tree.y))
+                    string = OneTimestepOneFile.addSelectedOutputs(
+                        self, tree, string, delimiter, growth_information)
+                    string += "\n"
+                    file.write(string)
+                    for growth_output in self.growth_outputs:
+                        del (growth_information[growth_output])
+                file.close()

58266e2cd5d10131a4d886fd2fc524efd8de9791
marie <mc.wimmler@gmx.de>
2023-06-08 11:33:37 +0200

    [doc] update directories to be ignored in doc-built

--- Benchmarks/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-@author: mcwimm
-"""
-
-# Exclude folder from automatic documentation as OGS import causes trouble
-__pdoc__ = {'ExampleSetups': False,
-            'ModuleBenchmarks.BelowgroundCompetition.OGSLargeScale3D': False}
-

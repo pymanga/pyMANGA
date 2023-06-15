722e8a4141018cf3cae5148f84570d78487aeefd
marie <mc.wimmler@gmx.de>
2023-06-15 11:12:51 +0200

    [doc] remove docstring in resource inits

--- ResourceLib/BelowGround/Generic/__init__.py
+++ ResourceLib/BelowGround/Generic/__init__.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Nov  8 15:25:03 2018
-
-@author: bathmann
-"""
-
 from .OGSLargeScale3DExternal import OGSLargeScale3DExternal
 from .NetworkOGSLargeScale3DExternal import NetworkOGSLargeScale3DExternal
 
--- ResourceLib/BelowGround/Individual/SZoiFixedSalinity/__init__.py
+++ ResourceLib/BelowGround/Individual/SZoiFixedSalinity/__init__.py
@@ -1,8 +1,3 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-@date: 2022-Today
-@author: marie-christin.wimmler@tu-dresden.de
-"""
-
 from .SZoiFixedSalinity import SZoiFixedSalinity

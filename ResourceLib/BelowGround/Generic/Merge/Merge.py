db82ee6ed1eae8dd28baab6619e1690279162b03
marie <mc.wimmler@gmx.de>
2023-08-02 00:40:40 +0200

    [RL] add merge class to combine multiple below ground resource modules

---
+++ ResourceLib/BelowGround/Generic/Merge/Merge.py
@@ -0,0 +1,70 @@
+import numpy as np
+from ResourceLib import ResourceModel
+
+
+class Merge(ResourceModel):
+    def __init__(self, args):
+        """
+        Below-ground resource concept.
+        Args:
+            args: Merge module specifications from project file tags
+        """
+        case = args.find("type").text
+        print("Initiate belowground competition of type " + case + ".")
+
+        try:
+            modules = args.find("modules").text
+        except AttributeError:
+            print("Error: No module specifications found.")
+
+        self.bg_concepts = []
+        for module in modules:
+            if module == "FixedSalinity":
+                from ResourceLib.BelowGround.Individual.FixedSalinity import FixedSalinity
+                self.bg_concepts.append(FixedSalinity(args))
+            elif module == "FON":
+                from ResourceLib.BelowGround.Individual.FON import FON
+                self.bg_concepts.append(FON(args))
+            elif module == "OGSLargeScale3D":
+                from ResourceLib.BelowGround.Individual.OGSLargeScale3D import OGSLargeScale3D
+                self.bg_concepts.append(OGSLargeScale3D(args))
+            elif module == "FON":
+                from ResourceLib.BelowGround.Individual.OGSWithoutFeedback import OGSWithoutFeedback
+                self.bg_concepts.append(OGSWithoutFeedback(args))
+            elif module == "SymmetricZOI":
+                from ResourceLib.BelowGround.Individual.SymmetricZOI import SymmetricZOI
+                self.bg_concepts.append(SymmetricZOI(args))
+            elif module == "Network":
+                from ResourceLib.BelowGround.Network.Network import Network
+                self.bg_concepts.append(Network(args))
+            elif module == "NetworkFixedSalinity":
+                from ResourceLib.BelowGround.Network.NetworkFixedSalinity import NetworkFixedSalinity
+                self.bg_concepts.append(NetworkFixedSalinity(args))
+            elif module == "NetworkOGS":
+                from ResourceLib.BelowGround.Network.NetworkOGS import NetworkOGS
+                self.bg_concepts.append(NetworkOGS(args))
+
+    def prepareNextTimeStep(self, t_ini, t_end):
+        for bg_concept in self.bg_concepts:
+            bg_concept.prepareNextTimeStep(t_ini, t_end)
+
+    def addPlant(self, plant):
+        for bg_concept in self.bg_concepts:
+            bg_concept.addPlant(plant)
+
+    def calculateBelowgroundResources(self):
+        """
+        Calculate a growth reduction factor for each tree based on specified modules and multiplies the factor of
+        all modules with each other.
+        Sets:
+            numpy array of shape(number_of_trees)
+        """
+        bg = []
+        for bg_concept in self.bg_concepts:
+            bg_concept.calculateBelowgroundResources()
+            bg.append(bg_concept.getBelowgroundResources())
+        bg = np.array(bg).transpose()
+        bg = list(map(np.prod, bg))
+
+        self.belowground_resources = bg
+
--- ResourceLib/BelowGround/Generic/__init__.py
+++ ResourceLib/BelowGround/Generic/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from .OGSLargeScale3DExternal import OGSLargeScale3DExternal
 from .NetworkOGSLargeScale3DExternal import NetworkOGSLargeScale3DExternal
-
+from .Merge import Merge
 
 # Exclude folder from automatic documentation as OGS import causes trouble
 __all__ = {'OGSLargeScale3DExternal.runModel': False}

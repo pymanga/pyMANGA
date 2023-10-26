718fc2700e17e3aa1edea4c553416916b5802f08
marie <mc.wimmler@gmx.de>
2023-10-26 11:15:29 +0200

    [RS] add intro text for generic

--- ResourceLib/BelowGround/Generic/__init__.py
+++ ResourceLib/BelowGround/Generic/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Collection of below-ground resource modules that allow for example the use of pyMANGA as boundary conditions of another model or to combine different resource modules.
+"""
 from .OGSExternal import OGSExternal
 from .NetworkOGSExternal import NetworkOGSExternal
 from .Merge import Merge

7d4f687b9c01b8da5d0b087058d0592b18328c1a
jardner1 <jardner1@uni-potsdam.de>
2024-02-08 11:10:36 +0100

    [radiation] update & moved to aboveground

--- ResourceLib/AboveGround/SolarRadiation/SolarRadiation.py
+++ ResourceLib/AboveGround/SolarRadiation/SolarRadiation.py
@@ -4,6 +4,7 @@
 @date: 2024-January
 @author: github.com/rbnmj/
 """
+<<<<<<< HEAD
 import numpy as np
 from scipy.optimize import curve_fit
 from ResourceLib import ResourceModel
@@ -120,10 +121,22 @@ class SolarRadiation(ResourceModel):
         Basic sine function skeleton to fit to the monthly net radiation.
         """
         return amplitude * np.sin(2 * np.pi * frequency * t + phase_shift) + vertical_shift
+=======
+import pysolar.solar as ps
+import datetime
+import numpy as np
+from ResourceLib import ResourceModel
+
+class SolarRadiation(ResourceModel):
+    def __init__(self, args):
+        case = args.find("SolarRadiation").text
+        self.getInputParameters(args)
+>>>>>>> cd15d636 ([radiation] update & moved to aboveground)
 
     def getInputParameters(self, args):
         tags = {
             "prj_file": args,
+<<<<<<< HEAD
             "required": ["type", "latitude", "tmin", "tmax"],
             "optional": ["albedo", "altitude", "variation"]
         }
@@ -172,3 +185,33 @@ class SolarRadiation(ResourceModel):
 
     def addPlant(self, plant):
         pass
+=======
+            "required": ["radiation", "date", "latitude", "longitude"]
+        }
+        super().getInputParameters(**tags)
+        self._date = datetime.datetime(*self.date, tzinfo=datetime.timezone.utc)
+        self._latitude = self.latitude
+        self._longitude = self.longitude
+
+    def calculateRadiation(self):
+        """ 
+        Calculates the daily mean solar irradiance (W/m2) for a given location and date.  
+        Clear sky daily mean solar irradiance is assumed to be 1000 W/m2.  
+        Calculating solar irradiance over 24 hours prevents time zone issues.  
+        """
+       
+        irradiance = []
+        for i in range(0,24):
+            date = self._date + datetime.timedelta(hours=i)
+            altitude = ps.get_altitude(self._latitude, self._longitude, date)
+            irradiance.append(ps.radiation.get_radiation_direct(date, altitude))
+
+        # Daily mean solar irradiance
+        daily_rad = np.mean(irradiance)
+
+        # Scale to maximal daily mean solar irradiance
+        max_rad = 1000 # on a clear day
+        rad_scaled = daily_rad / max_rad
+
+        return rad_scaled
+>>>>>>> cd15d636 ([radiation] update & moved to aboveground)

0ebf4d3d2512d82fac771a6c3fd29a4c959e3c33
jardner1 <jardner1@uni-potsdam.de>
2024-03-06 11:17:10 +0100

    [radiation] moved init to iniAbovegroundResourceConcept

--- ProjectLib/Project.py
+++ ProjectLib/Project.py
@@ -120,29 +120,6 @@ class MangaProject:
         self.aboveground_resource_concept = createAC(arg)
         print("Above-ground resources: {}.".format(case))
 
-    def getSolarRadiationConcept(self):
-        """
-        Get radiation object.
-        Returns:
-            class
-        """
-        return self.radiation_concept
-    
-    def iniSolarRadiationConcept(self):
-        """
-        Initialize radiation concept.
-        Sets:
-            class
-        """
-        arg = self.args["radiation"]
-        case = arg.find("type").text
-        if case == "SolarRadiation":
-            from ResourceLib.SolarRadiation.SolarRadiation import SolarRadiation as createSR
-        else:
-            raise KeyError("Solar radiation not implemented.")
-        self.solar_radiation_concept = createSR(arg)
-        print("Solar radiation: {}.".format(case))
-
     def getPlantDynamicConcept(self):
         """
         Get plant model object.

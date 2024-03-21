a60c861e7e58b0cbd540b59e1734c2486a267643
jardner1 <jardner1@uni-potsdam.de>
2024-03-21 10:49:27 +0100

    [radiation] rebase check + small fixes

--- ResourceLib/AboveGround/SolarRadiation/SolarRadiation.md
+++ ResourceLib/AboveGround/SolarRadiation/SolarRadiation.md
@@ -1,19 +1,16 @@
-The solar radiation above-ground resource concept.  
+# Description
 
-This concept provides a above-ground resource factor calculated from the net solar radiation.  
-Calculations are based on the FAO guidelines for computing crop water requirements [Allen et al. 1998](https://agris.fao.org/search/en/providers/122621/records/647231eb53aa8c896301eadc).  
-The calculations are strongly inspired by the now deprecated python package [PyETo](https://github.com/woodcrafty/PyETo).  
-The solar radiation factor represents the difference between incoming net solar radiation and the outgoing net longwave radiation based on latitude and temperature. Further, default assumptions for albedo, altitude, and variation are made but can also be provided as input in the project file.  
+The solar radiation above-ground resource concept.  
+This concept provides a above-ground resource factor calculated from the net solar radiation. Calculations are based on the FAO guidelines for computing crop water requirements [Allen et al. 1998](https://agris.fao.org/search/en/providers/122621/records/647231eb53aa8c896301eadc). The code is strongly inspired by the now deprecated python package [PyETo](https://github.com/woodcrafty/PyETo). The solar radiation used for the above-ground resources represents the difference between incoming net solar radiation and the outgoing net longwave radiation based on latitude and temperature. Further, default assumptions for albedo, altitude, and variation are made, but can also be provided as input in the project file.  
 
 Calculations are simplified by only needing yearly maximum and minimum temperature to reduce required user input. Results do not change significantly when using yearly temperatures compared to monthly temperatures. Each time step is then estimated from a sine curve that is fitted to the solar radiation data. By doing so, all calculations are done during the initialisation and do not require any further calculations during the simulation.  
 
-Examples:  
+# Usage 
 
 ```xml
     <resources>
         <aboveground>
             <type> SolarRadiation </type>
-            <day_of_year> 180 </day_of_year>
             <latitude> 0.65 </latitude>
             <tmin>24.33</tmin>
             <tmax>30.7</tmax>
@@ -23,3 +20,22 @@ Examples:
         </belowground>
     </resources>
 ```
+
+# Attributes
+
+* `type` (string): "SolarRadiation"
+* `latitude` (float): Latitude of the location in radians.
+* `tmin` (float): Minimum temperature in degrees Celsius.
+* `tmax` (float): Maximum temperature in degrees Celsius.
+* `albedo` (float): (optional) Albedo of the surface. Default: 0.23.
+* `altitude` (float): (optional) Altitude of the location in meters. Default: 0.
+* `noise` (float): (optional) Noise factor for the solar radiation. Default: Calculations explained in **Process overview** below. 
+
+# Value
+
+This factor is used to calculate the above-ground resources for the simulation. It is a float value between 0 and 1.  
+
+# Details 
+
+## Purpose
+
--- ResourceLib/AboveGround/SolarRadiation/SolarRadiation.py
+++ ResourceLib/AboveGround/SolarRadiation/SolarRadiation.py
@@ -4,10 +4,6 @@
 @date: 2024-January
 @author: github.com/rbnmj/
 """
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import numpy as np
 from scipy.optimize import curve_fit
 from ResourceLib import ResourceModel
@@ -43,7 +39,7 @@ class SolarRadiation(ResourceModel):
         Sine parameters are then used to estimate net radiation over the course of the year.
         Variation is drawn from a normal distribution. 
         Normal distribution mean is either taken from user input or based on the standard deviation of the net radiation estimate.
-        Default variation takes into account a more stable moving average for summer months based on Barr et al. 2014.
+        Default noise takes into account a more stable moving average for summer months based on Barr et al. 2014.
         Final net radiation is clipped at 0 to avoid negative net radiation and at the clear sky maximum net radiation.
         """
         # calculate monthly radiation
@@ -60,10 +56,11 @@ class SolarRadiation(ResourceModel):
             # inverse relative distance Earth-Sun based on doy
             ird = 1 + (0.033*np.cos((2*np.pi/365)*doy))
             # extraterrestrial radiation
+            solarconstant = 0.0820  # MJ m-2 min-1
             r_a_temp1 = ((24*60)/np.pi)
             r_a_temp2 = w_s * np.sin(self._latitude) * np.sin(d)
             r_a_temp3 = np.cos(self._latitude) * np.cos(d) * np.sin(w_s)
-            r_a = r_a_temp1 * 0.0820 * ird * (r_a_temp2 + r_a_temp3)
+            r_a = r_a_temp1 * solarconstant * ird * (r_a_temp2 + r_a_temp3)
             # incoming solar radiation
             r_s = (0.7*r_a)-4  # assuming island #(0.25 + (0.5 * N))*R_a
             # net shortwave radiation
@@ -73,7 +70,8 @@ class SolarRadiation(ResourceModel):
             # clear sky radiation
             r_so = (0.00002 * self._altitude + 0.75)*r_a
             # net longwave radiation
-            r_nl = 0.000000004903*(((self._tmax)**4+(self._tmin)**4)/2) * \
+            boltzmann = 0.000000004903 # Stefan Boltzmann constant [MJ K-4 m-2 day-1]
+            r_nl = boltzmann*(((self._tmax)**4+(self._tmin)**4)/2) * \
                 (0.34-(0.14*np.sqrt(e_a)))*(1.35*(r_s/r_so)-0.35)
             # daily net radiation
             r_n = r_ns - r_nl
@@ -90,18 +88,18 @@ class SolarRadiation(ResourceModel):
             np.arange(1, 366), *self.sinus_params)
         self.net_rad_max = max(self.net_rad_365)
 
-        # add variation
+        # add noise
         try:
-            # user input variation
-            self._variation
+            # user input noise
+            self._noise
             noise = np.random.normal(
-                scale=self._variation, size=self.net_rad_365.shape)
+                scale=self._noise, size=self.net_rad_365.shape)
             self.net_rad_365_noise = self.net_rad_365 + noise
             self.net_rad_365_clipped = np.clip(
                 self.net_rad_365_noise, 0, self.net_rad_365)
         # use standard deviation std as default
         except:
-            # default variation
+            # default noise
             noise = np.random.normal(
                 scale=0.5*np.std(self.net_rad_365), size=self.net_rad_365.shape)
             self.net_rad_365_noise = self.net_rad_365 + noise
@@ -111,7 +109,7 @@ class SolarRadiation(ResourceModel):
                 scale=5*np.std(self.net_rad_365[104:250]), size=self.net_rad_365[104:250].shape)
             self.net_rad_365_noise[104:250] -= + noise2
             # clip noise
-            # noise can only add "negative variability" as net radiation is calculated from a theoretical clear sky maximum
+            # noise can only add "negative noise" as net radiation is calculated from a theoretical clear sky maximum
             # net radiation can not be negative so we clip the noise at 0
             self.net_rad_365_clipped = np.clip(
                 self.net_rad_365_noise, 0, self.net_rad_365)
@@ -124,44 +122,12 @@ class SolarRadiation(ResourceModel):
         Basic sine function skeleton to fit to the monthly net radiation.
         """
         return amplitude * np.sin(2 * np.pi * frequency * t + phase_shift) + vertical_shift
-=======
-import pysolar.solar as ps
-=======
-import pyeto
->>>>>>> a263e7b4 ([radiation] pyeto implementation)
-=======
->>>>>>> e8a009eb ([radiation] removed extra packages)
-import datetime
-=======
->>>>>>> 14136542 ([radiation] updated initialisation)
-import numpy as np
-from scipy.optimize import curve_fit
-from ResourceLib import ResourceModel
 
-
-class SolarRadiation(ResourceModel):
-    def __init__(self, args):
-        """
-        Above-ground resource concept.
-        Solar radiation model based on Allen et al. 1998.
-        Requires daily minimum and maximum temperature, latitude and day of the year.
-        Args:
-            args (lxml.etree._Element): Module specifications from project file tags
-        """
-        case = args.find("type").text
-        self.getInputParameters(args)
-<<<<<<< HEAD
->>>>>>> cd15d636 ([radiation] update & moved to aboveground)
-
-<<<<<<< HEAD
     def getInputParameters(self, args):
         tags = {
             "prj_file": args,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
             "required": ["type", "latitude", "tmin", "tmax"],
-            "optional": ["albedo", "altitude", "variation"]
+            "optional": ["albedo", "altitude", "noise"]
         }
         super().getInputParameters(**tags)
         if -np.pi/2 <= self.latitude <= np.pi/2:
@@ -181,7 +147,7 @@ class SolarRadiation(ResourceModel):
         except:
             pass
         try:
-            self._variation = self.variation
+            self._noise = self.noise
         except:
             pass
 
@@ -205,262 +171,8 @@ class SolarRadiation(ResourceModel):
             self._doy += 1
         else:
             self._doy = 1
-
-    def addPlant(self, plant):
-        pass
-=======
-            "required": ["radiation", "date", "latitude", "longitude"]
-=======
-            "required": ["radiation", "day_of_year", "latitude", "tmin", "tmax"]
->>>>>>> a263e7b4 ([radiation] pyeto implementation)
-=======
-            "required": ["type", "day_of_year", "latitude", "tmin", "tmax"]
->>>>>>> df860a45 ([radiation] fixed input parameters)
-        }
-        super().getInputParameters(**tags)
-        self._doy = self.day_of_year #day of the year
-        self._latitude = self.latitude
-        self._tmin = self.tmin
-        self._tmax = self.tmax
-
-    def calculateMonthlyRadiation(self):
-        """ 
-        Calculates daily net radiation based on Allen et al 1998.
-        """
-=======
-=======
-        try:
-            self._albedo
-        except:
-            # defaul albedo assumes green vegetation (Allen et al, 1998)
-            self._albedo = 0.23
-        try:
-            self._altitude
-        except:
-            # default altitude assumes sea level
-            self._altitude = 0
-        self._doy = 1
-        self.calculateNetRadiation()
-
-    def calculateNetRadiation(self):
-        """
-        Calculates net radiation for each month and fits a sine curve to the data.
-        Sine parameters are then used to estimate net radiation over the course of the year.
-        Variation is drawn from a normal distribution. 
-        Normal distribution mean is either taken from user input or based on the standard deviation of the net radiation estimate.
-        Default variation takes into account a more stable moving average for summer months based on Barr et al. 2014.
-        Final net radiation is clipped at 0 to avoid negative net radiation and at the clear sky maximum net radiation.
-        """
->>>>>>> d47fe42b ([radiation] fixes and documentation)
-        # calculate monthly radiation
->>>>>>> 14136542 ([radiation] updated initialisation)
-        self.monthly_net_rad = []
-        self.doy15 = [15, 46, 74, 105, 135, 166, 196,
-                      227, 258, 288, 319, 349]  # 15th of each month
-        for doy in self.doy15:
-            # solar declination
-            d = 0.409*np.sin((2*np.pi/365)*doy-1.39)
-            # sunset hour angle
-            # if >= 1, 24hrs of daylight, if <= -1, 24hrs of darkness (see pyeto docs)
-            w_s_temp = -np.tan(self._latitude)*np.tan(d)
-            w_s = np.arccos(min(max(w_s_temp, -1), 1))
-            # inverse relative distance Earth-Sun based on doy
-            ird = 1 + (0.033*np.cos((2*np.pi/365)*doy))
-            # extraterrestrial radiation
-            r_a_temp1 = ((24*60)/np.pi)
-            r_a_temp2 = w_s * np.sin(self._latitude) * np.sin(d)
-            r_a_temp3 = np.cos(self._latitude) * np.cos(d) * np.sin(w_s)
-            r_a = r_a_temp1 * 0.0820 * ird * (r_a_temp2 + r_a_temp3)
-            # incoming solar radiation
-            r_s = (0.7*r_a)-4  # assuming island #(0.25 + (0.5 * N))*R_a
-            # net shortwave radiation
-            r_ns = (1-self._albedo)*r_s
-            # actual vapor pressure (substract 2 deg C from tmin in arid/semi arid areas)
-            e_a = 0.611 * np.exp((17.27 * self._tmin) / (self._tmin + 237.3))
-            # clear sky radiation
-            r_so = (0.00002 * self._altitude + 0.75)*r_a
-            # net longwave radiation
-            r_nl = 0.000000004903*(((self._tmax)**4+(self._tmin)**4)/2) * \
-                (0.34-(0.14*np.sqrt(e_a)))*(1.35*(r_s/r_so)-0.35)
-            # daily net radiation
-            r_n = r_ns - r_nl
-            self.monthly_net_rad.append(r_n)
-
-        # fit sine curve to monthly net radiation
-        # popt is an array of optimal values for the sine parameters so that sum of squared residuals is minimized e.g. best fit
-        # popt contains amplitude, frequency, phase_shift and vertical_shift to pass to sineFunction
-        popt = curve_fit(self.sineFunction, self.doy15,
-                         self.monthly_net_rad)[0]
-        self.sinus_params = popt
-
-        self.net_rad_365 = self.sineFunction(
-            np.arange(1, 366), *self.sinus_params)
-        self.net_rad_max = max(self.net_rad_365)
-
-        # add variation
-        try:
-            # user input variation
-            self._variation
-            noise = np.random.normal(
-                scale=self._variation, size=self.net_rad_365.shape)
-            self.net_rad_365_noise = self.net_rad_365 + noise
-            self.net_rad_365_clipped = np.clip(
-                self.net_rad_365_noise, 0, self.net_rad_365)
-        # use standard deviation std as default
-        except:
-            # default variation
-            noise = np.random.normal(
-                scale=0.5*np.std(self.net_rad_365), size=self.net_rad_365.shape)
-            self.net_rad_365_noise = self.net_rad_365 + noise
-            # moving average is nearly constant for summer months (Barr et al 2014)
-            # add higher noise for summer months
-            noise2 = np.random.normal(
-                scale=5*np.std(self.net_rad_365[104:250]), size=self.net_rad_365[104:250].shape)
-            self.net_rad_365_noise[104:250] -= + noise2
-            # clip noise
-            # noise can only add "negative variability" as net radiation is calculated from a theoretical clear sky maximum
-            # net radiation can not be negative so we clip the noise at 0
-            self.net_rad_365_clipped = np.clip(
-                self.net_rad_365_noise, 0, self.net_rad_365)
-
-        # final net radiation
-        self.net_rad_year = self.net_rad_365_clipped
-
-    def sineFunction(self, t, amplitude, frequency, phase_shift, vertical_shift):
-        """
-        Basic sine function skeleton to fit to the monthly net radiation.
-        """
-        return amplitude * np.sin(2 * np.pi * frequency * t + phase_shift) + vertical_shift
-
-    def getInputParameters(self, args):
-        tags = {
-            "prj_file": args,
-            "required": ["type", "latitude", "tmin", "tmax"],
-            "optional": ["albedo", "altitude", "variation"]
-        }
-        super().getInputParameters(**tags)
-        if -np.pi/2 <= self.latitude <= np.pi/2:
-            self._latitude = self.latitude
-            print(
-                "If latitude input ranges from -pi/2 to pi/2 pyMANGA assumes radians.")
-        else:
-            self._latitude = np.deg2rad(self.latitude)
-        self._tmin = self.tmin
-        self._tmax = self.tmax
-        try:
-            self._albedo = self.albedo
-        except:
-            pass
-        try:
-            self._altitude = self.altitude
-        except:
-            pass
-        try:
-            self._variation = self.variation
-        except:
-            pass
-
-    def calculateAbovegroundResources(self):
-        """ 
-        Accesses net radiation for current day and scales it between 0 and 1
-        to make it accessible for the resource model.
-        """
-        # pull radiation for current day
-        self.net_rad = self.net_rad_year[self._doy-1]
-
-        # scale net radiation between 0 and 1
-        self.net_rad_scale = self.net_rad / self.net_rad_max
-        self.aboveground_resources = self.net_rad_scale
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        # solar shortwave radiation
-        # An island is defined as a land mass with width perpendicular to the coastline <= 20 km. 
-        # This method is only applicable for low altitudes (0-100 m) and monthly calculations.
-        solRad = pyeto.sol_rad_island(etRad)
-
-        # Default value is 0.23, which is the value used by the FAO for a short grass reference crop.
-        # A green vegetation over has an albedo of about 0.20-0.25 (Allen et al, 1998).
-        albedo = 0.23 
-
-        # net incoming shortwave rad.
-        netInSWRad = pyeto.net_in_sol_rad(solRad, albedo=albedo)
-
-        ## net outgoing longwave rad estimated from:
-
-        # clear sky radiation
-        altitude = 0 # estimate for sea level
-        cSRad = pyeto.cs_rad(altitude, etRad)
-
-        # actual vapor pressure (usually from tdeq or humdity data but can be estimated from tmin)
-        # recommended to substract 2 °C from tmin for arid areas (Allen et al, 1998; Annex 6)
-        aVP = pyeto.avp_from_tmin(self._tmin)
-
-        # net outgoing longwave rad.
-        netOutLWRad = pyeto.net_out_lw_rad(self._tmin, self._tmax, solRad, cSRad, aVP)
-
-        ## daily net radiation
-        # Based on equation 40 in Allen et al (1998)
-        dailyNetRad = pyeto.net_rad(netInSWRad, netOutLWRad)
-
-        return dailyNetRad
-
-        # Scale to maximal daily mean solar irradiance
-        # max_rad = 
-        # rad_scaled = dailyNetRad / max_rad
-
-<<<<<<< HEAD
-        return rad_scaled
->>>>>>> cd15d636 ([radiation] update & moved to aboveground)
-=======
-        # return rad_scaled
-
-    def calculateRadiation_yearly(self):
-        yearlyNetRad = []
-        for self._doy in range(1, 366):
-            dailyNetRad = self.calculateRadiation()
-            yearlyNetRad.append(dailyNetRad)
-        # plt.plot(range(0,365), yearlyNetRad)
-        # plt.xlabel('Day of the year')
-        # plt.ylabel('Net radiation (MJ m-2 day-1)')
-        # plt.show()
->>>>>>> a263e7b4 ([radiation] pyeto implementation)
-=======
-        return self.net_rad
-    
-    def factorDailyRadiaton(self):
-        """ 
-        Scales radiation between 0 and 1 to make it accessable for the resource model.
-        """
-        self.net_rad_factor = self.net_rad / max(self.net_rad)
-        return self.daily_net_rad_factor
->>>>>>> e8a009eb ([radiation] removed extra packages)
-=======
-        # factor
-        self.net_rad_factor = self.net_rad / self.net_rad_max
-        self.aboveground_resources =  self.net_rad_factor
-    
-=======
->>>>>>> d47fe42b ([radiation] fixes and documentation)
-    def prepareNextTimeStep(self, t_ini, t_end):
-        self.radiation = []
-        self.t_ini = t_ini
-        self.t_end = t_end
-        if self._doy < 365:
-            self._doy += 1
-        else:
-            self._doy = 1
+        self.no_plants = 0
 
     def addPlant(self, plant):
         # Count the number of plants in the system
-<<<<<<< HEAD
-        # self.no_plants += 1
-        pass
-<<<<<<< HEAD
->>>>>>> 14136542 ([radiation] updated initialisation)
-=======
->>>>>>> d47fe42b ([radiation] fixes and documentation)
-=======
         self.no_plants += 1
->>>>>>> b27f1406 ([radiation] oversight + docs)

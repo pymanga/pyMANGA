309a742b8c53705645eb341d94a0e1f32a3e54b8
marie <mc.wimmler@gmx.de>
2024-03-28 18:49:25 +0100

    [doc] restructure landing page

--- __init__.py
+++ __init__.py
@@ -1,23 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-PYthon Models for AGeNt-based resource GAthering (pyMANGA) is a collection of models describing vegetation population dynamics from first principles.
-pyMANGA is modular, and descriptions of individual tree growth, resource availability and competition dynamics can be quickly interchanged.
-
-The documentation of a module consists of the following parts
-1. module description
-2. specification of tags to be used in the pyMANGA project file (if applicable)
-3. source code documentation.
-
-*Note*
-All parameters are defined in SI units unless indicated otherwise.
-
-For general information about the pyMANGA project and tutorials visit our website: http://pymanga.forst.tu-dresden.de/
-
-CoreTeam:
-    @[jbathmann](https://github.com/jbathmann)
-    @[mcwimm](https://github.com/mcwimm/)
-    @[jvollhueter](https://github.com/jvollhueter)
+.. include:: ./doc_page.md
 """
 import os, sys
 

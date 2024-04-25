db57b8a645e3bb34290dd1dd78a545256ed6b2f5
marie <mc.wimmler@gmx.de>
2024-04-25 16:48:48 +0200

    [PL] bugfix in kiwi species file

--- PopulationLib/Species/AvicenniaKiwi/AvicenniaKiwi.py
+++ PopulationLib/Species/AvicenniaKiwi/AvicenniaKiwi.py
@@ -4,13 +4,12 @@
 
 def createPlant():
     """
-    Constructor containing species-specific parameters and the initial geometry of a plant required by the Kiwi plant module.
+    Constructor containing species-specific parameters and the initial geometry of a plant required by the Jabowa plant module.
     Returns:
         multiple dict
     """
     geometry = {}
     parameter = {}
-    geometry["height"] = 0                              # m
     geometry["r_stem"] = 0.005                          # m
     parameter["salt_effect_d"] = -0.18
     parameter["salt_effect_ui"] = 72
@@ -24,8 +23,11 @@ def createPlant():
     # Zone of influence is used as proxy for root plate and crown radius
     # Scaling dbh to zone of influence (ZOI) based on eq. 1 in
     # Berger & Hildenbrandt 2000
-    geometry["r_bg"] = parameter["a_zoi_scaling"] * geometry["r_stem"]*0.5
+    geometry["r_bg"] = parameter["a_zoi_scaling"] * geometry["r_stem"]**0.5
     geometry["r_ag"] = geometry["r_bg"]
+    dbh_cm = geometry["r_stem"] * 200                   # cm
+    height_cm = (137 + parameter["b2"] * dbh_cm - parameter["b3"] * dbh_cm ** 2)
+    geometry["height"] = height_cm / 100                # m
     # resource module FixedSalinity
     parameter["r_salinity"] = "forman"
     # resource module FON

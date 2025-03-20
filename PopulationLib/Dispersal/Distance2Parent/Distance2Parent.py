73d92615b8329d3f21ea33f5a17d34824006633a
ChrisWudel <chris-wudel@web.de>
2025-03-20 09:25:34 +0100

    [PL] Adjusted benchmarks to increase tree mortality, reduce CSV storage volume, and added gamma distribution for Distance2Parent

--- PopulationLib/Dispersal/Distance2Parent/Distance2Parent.py
+++ PopulationLib/Dispersal/Distance2Parent/Distance2Parent.py
@@ -83,6 +83,10 @@ class Distance2Parent:
 
             if self.distribution in "exponential":
                 return np.random.exponential(scale=self.scale, size=number_of_plants)
+
+            if self.distribution in "gamma":
+                return np.random.gamma(shape=self.loc, scale=self.scale, size=number_of_plants)
+
         except AttributeError:
             print("ERROR: A parameter defining the probability density function is missing. "
                   "Check the documentation of <dispersal>.")

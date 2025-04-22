9f38f0bdbed1b79b4a9a90455d6fe137b6c4cd15
ChrisWudel <chris-wudel@web.de>
2025-04-22 13:55:26 +0200

    [RL] Fix Asymmetric ZOI bug and add NaN check for aboveground and belowground resources
    [RL] Fix Asymmetric ZOI bug and add NaN check for aboveground and belowground resources
    
    [RL] Fix Asymmetric ZOI bug and add NaN check for aboveground and belowground resources
    
    [RL] Fix Asymmetric ZOI bug and add NaN check for aboveground and belowground resources
    
    [RL] Fix Asymmetric ZOI bug and add NaN check for aboveground and belowground resources
    
    [RL] Fix Asymmetric ZOI bug and add NaN check for aboveground and belowground resources

--- PopulationLib/Dispersal/Distance2Parent/Distance2Parent.py
+++ PopulationLib/Dispersal/Distance2Parent/Distance2Parent.py
@@ -1,7 +1,6 @@
 import numpy as np
 from ProjectLib import helpers as helpers
 
-
 class Distance2Parent:
     """
     Distance2Parent dispersal module
@@ -18,7 +17,7 @@ class Distance2Parent:
         tags = {
             "prj_file": args,
             "required": ["type", "distribution"],
-            "optional": ["scale", "loc", "high"]
+            "optional": ["scale", "loc", "shape", "high", "low", "a"]
         }
         myself = super(Distance2Parent, self)
         helpers.getInputParameters(myself, **tags)
@@ -32,9 +31,18 @@ class Distance2Parent:
         if not hasattr(self, "loc"):
             self.loc = 0
             print("INFO: Default value for <dispersal><FixedRate><loc> is used. Default: 0")
+        if not hasattr(self, "shape"):
+            self.shape = 1
+            print("INFO: Default value for <dispersal><FixedRate><shape> is used. Default: 1")
         if not hasattr(self, "high"):
             self.high = 1
             print("INFO: Default value for <dispersal><FixedRate><high> is used. Default: 1")
+        if not hasattr(self, "low"):
+            self.low = 0
+            print("INFO: Default value for <dispersal><FixedRate><low> is used. Default: 0")
+        if not hasattr(self, "a"):
+            self.a = 1
+            print("INFO: Default value for <dispersal><FixedRate><a> is used. Default: 1")
 
     def getPositions(self, number_of_plants, plants):
         """
@@ -46,11 +54,11 @@ class Distance2Parent:
             dict
         """
         if np.isscalar(number_of_plants):
-            print("ERROR: it seems <production><per_individual> is not defined.")
-            exit()
-        number_of_plants = number_of_plants["per_individual"]
+            print("ERROR: It seems <production><per_individual> is not defined.")
 
+        number_of_plants = number_of_plants["per_individual"]
         self.xi, self.yi = [], []
+
         for i in range(len(plants)):
             plant = plants[i]
             xp, yp = plant.getPosition()
@@ -64,14 +72,17 @@ class Distance2Parent:
         x = np.concatenate(self.xi) if self.xi else np.array([])
         y = np.concatenate(self.yi) if self.yi else np.array([])
 
+        # Model domain boundaries
         x1, x2 = float(self.x_1), float(self.x_2)
         y1, y2 = float(self.y_1), float(self.y_2)
+
         # Drop seeds that are outside the model domain
         idx = np.where(np.logical_or.reduce((x < x1, x > x2, y < y1, y > y2)))
 
         x = np.delete(x, idx)
         y = np.delete(y, idx)
 
+        # Return plant positions
         plant_positions = {"x": x, "y": y}
         return plant_positions
 
@@ -84,22 +95,22 @@ class Distance2Parent:
             list
         """
         try:
-            if self.distribution in "normal":
+            if self.distribution == "normal":
                 return np.random.normal(loc=self.loc, scale=self.scale, size=number_of_plants)
 
-            if self.distribution in "uniform":
-                return np.random.uniform(size=number_of_plants, high=self.high)
+            if self.distribution == "uniform":
+                return np.random.uniform(low=self.low, high=self.high, size=number_of_plants)
 
-            if self.distribution in "exponential":
+            if self.distribution == "exponential":
                 return np.random.exponential(scale=self.scale, size=number_of_plants)
 
-            if self.distribution in "gamma":
-                return np.random.gamma(shape=self.loc, scale=self.scale, size=number_of_plants)
+            if self.distribution == "gamma":
+                return np.random.gamma(shape=self.shape, scale=self.scale, size=number_of_plants)
 
-            if self.distribution in "weibull":
-                return np.random.weibull(a=self.scale, size=number_of_plants)
+            if self.distribution == "weibull":
+                return np.random.weibull(a=self.a, size=number_of_plants)
 
-            if self.distribution in "lognormal":
+            if self.distribution == "lognormal":
                 return np.random.lognormal(mean=self.loc, sigma=self.scale, size=number_of_plants)
 
         except AttributeError:

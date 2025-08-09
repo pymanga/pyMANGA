43c1b3a95f6b503955b875054559039e5dddcc94
jvollhueter <jonas.vollhueter>
2025-08-09 09:36:05 +0200

    New parametrization of saltmarsh default PFT

--- PopulationLib/Species/Saltmarsh/Saltmarsh.py
+++ PopulationLib/Species/Saltmarsh/Saltmarsh.py
@@ -5,14 +5,17 @@ def createPlant():
     geometry = {}
     parameter = {}
     geometry["r_ag"] = 0.05
-    geometry["h_ag"] = 0.1
+    geometry["r_ag_ic"] = 0.05
+    geometry["h_ag"] = 0.05
     geometry["r_bg"] = 0.05
-    geometry["h_bg"] = 0.1
-    parameter["maint_factor"] = 0.00002
-    parameter["growth_factor"] = 0.0000002
-    parameter['w_b_a'] = 0.7
-    parameter['w_ag'] = 0.2
-    parameter['w_bg'] = 0.6
+    geometry["r_bg_ic"] = 0.05
+    geometry["h_bg"] = 0.05
+    geometry['volume_ic'] = 0.0007853975
+    parameter["maint_factor"] = 4.5e-10
+    parameter["growth_factor"] = 6.6e-8
+    parameter['w_b_a'] = 0.5
+    parameter['w_ag'] = 0.5
+    parameter['w_bg'] = 0.5
     parameter["r_salinity"] = "forman"
     # resource module FON
     parameter["aa"] = 10
